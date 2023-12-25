ba64b2886288c9612cbbe3454dbfecde5954b7f3
alfisahr <alfin.andri@gmail.com>
2023-12-25 17:42:50 +0700

    Change appname to app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "NextJS-CICD-Test",
+      name: "app",
       cwd: "/var/frontend",
       script: "npm",
       args: "start",
--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,7 +3,7 @@ pm2 describe app > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start
+  pm2 start npm --name app -- start
 else
   pm2 reload app
 fi;
