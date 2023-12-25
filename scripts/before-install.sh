e92af8a30d6d7ccf6606eeb9eaeaced2109fc1b5
alfisahr <alfin.andri@gmail.com>
2023-12-25 20:05:35 +0700

    change node version

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,7 +3,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 16.14.2
+nvm install 18.17.0
 npm install pm2 -g
 
 ln -sf $(which npm) /usr/local/bin/npm
